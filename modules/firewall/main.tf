5a449faab1eb8942563dfb654b13cb3930209371
kakroo1981 <52753770+kakroo1981@users.noreply.github.com>
2021-05-23 23:04:43 +0530

    Update main.tf
    Test Integration of GitHub Repo and Google Cloud Build For Infra Provisioning Through Terraform Code

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# Test Integration of GitHub Repo and Google Cloud Build For Infra Provisioning Through Terraform Code
 
 locals {
   network = "${element(split("-", var.subnet), 0)}"
